aa12bb6a432a295a0ec2c8fa439f4f9a45b353d4
jkbh <jakob_h@outlook.de>
2023-06-21 22:56:40 +0200

    some comments and parameter changes

--- ue2/Uebung2.m
+++ ue2/Uebung2.m
@@ -1,12 +1,17 @@
+% Jakob Horbank
+% Melf Fritsch
+
+%% Schritt 1
+
 % Laden der Audiodatei
 filename = './data/female/00002.wav';
-[x, fs] = audioread(filename); % Einlesen der Audiodatei
-x = x(:, 1); % Falls das Audiosignal mehrere Kanäle hat, wählen Sie den gewünschten Kanal aus
+[x, fs] = audioread(filename);
+x = x(:, 1); % erster channel
 
 % Parameter für den Vocoder
-frameLength = 0.03; % Länge eines Frames in Sekunden
-overlap = 0.5; % Überlappungsfaktor der Frames
-order = 20; % Ordnung des LPC-Modells
+frameLength = 0.03;  
+overlap = 0.5; 
+order = 20; 
 
 % Fensterlänge und Überlappung in Abtastwerten berechnen
 frameLengthSamples = round(frameLength * fs);
@@ -63,24 +68,28 @@ y = y / max(abs(y));
 % Stimme laden
 voice = load("./data/femalevoice.mat").female;
 
-% Dictionary für lpc coeffs
+% Dict in die lpc coeffs für alle sample für jedes phonem gespeichert
+% werden
 lpcsPhonemDict = dictionary(string([]), cell([]));
 
+% Loop audiofiles
 for i = 1:10
     [x, fs] = audioread(['./data/female/', num2str(i, '%0.5d'), '.wav']);
     
     % Intervalle und dazugehörige phoneme laden
     voiceData = voice{2,i};
     numIntervals = size(voiceData, 1);    
-
+    
+    % Loop Intervalle für audiofile
     for j = 1:numIntervals
         interval = [voiceData{j, 1:2}];
         intervalSample = round(interval ./ 1e7 .* fs); % 100 nanoseconds to seconds to samples
         phonem = voiceData{j,3};
         
+        %LPC coeffs für aktuelles intervall
         lpcCoeffsPhonem = lpc(x(intervalSample(1) + 1:intervalSample(2)), order);
-    
-        % Build up dict with lpc coefficients for all phonem samples
+        
+        % Updaten des Dict
         if isKey(lpcsPhonemDict, phonem)
             lpcsPhonemDict{phonem} =  cat(1, lpcsPhonemDict{phonem}, lpcCoeffsPhonem);
         else
@@ -91,7 +100,7 @@ end
 
 numPhonems = numEntries(lpcsPhonemDict);
 
-% average over lpc coefficients
+% Für jedes Phonem im Dict die durschnittlichen LPC Koeff berechnen
 for i = 1:numPhonems
     phonems = keys(lpcsPhonemDict);
     phonem = phonems(i);
@@ -100,21 +109,24 @@ end
 
 %% Schritt 3
 
-%phonems = ["jh", "ey", "k", "ah", "b"];
-phonems = ["m", "ae", "el", "f"];
+phonems = ["jh", "ey", "k", "ah", "b"];
+%phonems = ["m", "ae", "el", "f"];
 
+% Parameter
 frameLengthSamples = 0.4 * fs;
-overlap = 0.65;s
+overlap = 0.65;
 overlapSamples = round(frameLengthSamples * overlap);
 outputLength = (length(phonems) - 1) * overlapSamples + frameLengthSamples;
 output = zeros(outputLength, 1);
 
 for i = 1:length(phonems)
+    % Excitation mischen aus stimmlos und stimmhaft
     excitationVoiceless = randn(frameLengthSamples, 1);
     excitationVoice = [1; zeros(frameLengthSamples - 1, 1)];
-    excitation = 0.97 * excitationVoice + 0.03 * excitationVoiceless;
+    excitation = 0.3 * excitationVoice + 0.01 * excitationVoiceless;
     voicedFrame = filter(1, lpcsPhonemDict{phonems(i)}, excitation);
     
+    % Overlapp add
     startIdx = (i - 1) * overlapSamples + 1;
     endIdx = startIdx + frameLengthSamples - 1;
     
